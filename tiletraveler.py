8847e34e49b65c8b90ceb7ec348f2a99d978f2a1
Aleksei Voronin <alekseivoronin@Alekseis-MacBook-Pro.local>
2019-09-17 14:13:55 +0000

    Starting code

--- tiletraveler.py
+++ tiletraveler.py
@@ -1,2 +1,13 @@
 #Start æltum að byrja að búa til grid af 3x3 og gefa tækifæðrið til að færa sig S fyrir suður W Vestur N fyrir norður og E fyrir austur og hann færist um einn eftir því hvað er valið
 #markmiðið verður að komast að leiðarenda í forritinu til að vinna leikin
+
+x = 1
+y = 1
+
+valid_direction = "nN"
+victory = False
+print("You can travel: (N)orth.")
+
+position = (x,y)
+
+
