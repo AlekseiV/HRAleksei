6fb457ad948aa7c2f2cd3542b20bac070a8c8ca9
therobert59 <roberthelgi1995@hotmail.com>
2019-09-17 14:58:07 +0000

    done and done

--- tiletraveler.py
+++ tiletraveler.py
@@ -107,3 +107,35 @@ while not last_tile:
             elif position == (3, 3):
                 valid_direction= "sSwW"
                 print("You can travel: (S)outh or (W)est.")
+
+        elif direction == "W" or direction == "w":
+            x -= 1
+            position =(x, y)
+            if position == (1, 1):
+                valid_direction = "nN" 
+                print("You can travel: (N)orth.")
+            elif position == (1, 2):
+                valid_direction= "nNeEsS"
+                print("You can travel: (N)orth or (E)ast or (S)outh.")
+            elif position == (1, 3):
+                valid_direction= "eEsS" 
+                print("You can travel: (E)ast or (S)outh.")
+            elif position == (2, 1):
+                valid_direction= "nN" 
+                print("You can travel: (N)orth.")
+            elif position == (2, 2):
+                valid_direction= "wWsS"
+                print("You can travel: (S)outh or (W)est.")
+            elif position == (2, 3): 
+                valid_direction= "eEwW"
+                print("You can travel: (E)ast or (W)est.")
+            elif position == (3, 1):
+                print("Victory!")
+                last_tile = True
+            elif position == (3, 2):
+                valid_direction= "nNsS" 
+                print("You can travel: (N)orth or (S)outh.")
+            elif position == (3, 3):
+                valid_direction= "sSwW"
+                print("You can travel: (S)outh or (W)est.")
+#Done biatch
