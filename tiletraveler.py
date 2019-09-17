0f8de6a94886b997b29aa87be37b53a166b1b1e2
therobert59 <roberthelgi1995@hotmail.com>
2019-09-17 14:02:03 +0000

    texti

--- tiletraveler.py
+++ tiletraveler.py
@@ -1,1 +1,2 @@
-#Start æltum að byrja að 
+#Start æltum að byrja að búa til grid af 3x3 og gefa tækifæðrið til að færa sig S fyrir suður W Vestur N fyrir norður og E fyrir austur og hann færist um einn eftir því hvað er valið
+#markmiðið verður að komast að leiðarenda í forritinu til að vinna leikin
